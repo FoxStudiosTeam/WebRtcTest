540df9f160b7a40f0e78d8ee83bdb2965ecfb86e
Danil <danil.saykin.1999@mail.ru>
2024-11-15 19:32:56 +0500

    cam component from webrtc official cite

---
+++ web/src/app/components/cam.tsx
@@ -0,0 +1,59 @@
+"use client"
+import { useEffect, useState } from "react";
+
+const constraints = {
+    audio: false,
+    video: true,
+};
+
+export default function Camera() {
+    const [stream, setStream] = useState<MediaStream | null>(null);
+    const [error, setError] = useState<string | null>(null);
+
+    const handleSuccess = (stream: MediaStream) => {
+        const video = document.querySelector("video") as HTMLVideoElement;
+        const videoTracks = stream.getVideoTracks();
+        console.log("Got stream with constraints:", constraints);
+        console.log(`Using video device: ${videoTracks[0].label}`);
+        setStream(stream);
+        video.srcObject = stream;
+    };
+
+    const handleError = (error: never) => {
+        if (error === "OverconstrainedError") {
+            setError(`OverconstrainedError: The constraints could not be satisfied by the available devices. Constraints: ${JSON.stringify(constraints)}`);
+        } else if (error=== "NotAllowedError") {
+            setError("NotAllowedError: Permissions have not been granted to use your camera and microphone.");
+        } else {
+            setError(`getUserMedia error: ${error}`);
+        }
+    };
+
+    const init = async () => {
+        try {
+            const stream = await navigator.mediaDevices.getUserMedia(constraints);
+            handleSuccess(stream);
+        } catch (e) {
+            handleError(e);
+        }
+    };
+
+    useEffect(() => {
+        return () => {
+            if (stream) {
+                const tracks = stream.getTracks();
+                tracks.forEach((track) => track.stop());
+            }
+        };
+    }, [stream]);
+
+    return (
+        <div id="container">
+            <video id="gum-local" autoPlay playsInline></video>
+            <button id="showVideo" onClick={init}>
+                Open camera
+            </button>
+            {error && <div id="errorMsg"><p>{error}</p></div>}
+        </div>
+    );
+}

241ad02772b18746268fb4474055c9fd92cb28f9
kaiva-morphin <kaivamorphin@gmail.com>
2024-11-17 13:24:08 +0300

    extended connector functional

--- connector-service/src/main/kotlin/ru/foxstudios/service/WsService.kt
+++ connector-service/src/main/kotlin/ru/foxstudios/service/WsService.kt
@@ -2,6 +2,7 @@ package ru.foxstudios.service
 
 import io.ktor.server.application.*
 import io.ktor.websocket.*
+import ru.foxstudios.controller.ClientPair
 import ru.foxstudios.controller.objectMapper
 import ru.foxstudios.domain.Message
 import ru.foxstudios.domain.MessageType
@@ -10,8 +11,9 @@ class WsService(var module : Application) {
     suspend fun resolveMove(
         message: Message,
         connections: HashMap<String, WebSocketSession>,
-        rooms: HashMap<String, HashSet<String>>,
+        rooms: HashMap<String, ClientPair>,
         client: String,
+        isOperator: Boolean,
         wsSession: WebSocketSession
     ) {
         try {
@@ -19,10 +21,15 @@ class WsService(var module : Application) {
                 MessageType.join -> {
                     if (message.room != null) {
                         if (!rooms.contains(message.room)) {
-                            rooms[message.room!!] = HashSet<String>()
+                            rooms[message.room!!] = ClientPair(null, null);
                         }
-                        rooms[message.room!!]!!.add(client)
-
+                        if (!rooms[message.room!!]!!.tryAdd(client, isOperator)) {
+                            val localMessage = Message(MessageType.disconnect, null, message.room!!)
+                            connections[client]?.send(objectMapper.writeValueAsString(localMessage))
+                            module.log.error("Already exists!")
+                            module.log.error(rooms.toString())
+                            return
+                        }
                         for (otherClient in rooms[message.room!!]!!) {
                             if (otherClient != client) {
                                 val localMessage = Message(MessageType.user_joined, null, message.room!!)
@@ -32,7 +39,15 @@ class WsService(var module : Application) {
                         }
                     }
                 }
-
+                MessageType.disconnect -> {
+                    for (otherClient in rooms[message.room!!]!!) {
+                        if (otherClient != client) {
+                            val localMessage = Message(MessageType.user_left, null, message.room!!)
+                            val resultMessage = objectMapper.writeValueAsString(localMessage)
+                            connections[otherClient]?.send(resultMessage)
+                        }
+                    }
+                }
                 else -> {
                     for (otherClient in rooms[message.room!!]!!) {
                         if (otherClient != client) {
@@ -45,6 +60,20 @@ class WsService(var module : Application) {
             }
         } catch (e: Exception) {
             println(e.message)
+        }
+    }
+    suspend fun handleDisconnect(
+        connections: HashMap<String, WebSocketSession>,
+        rooms: HashMap<String, ClientPair>,
+        client: String,
+        isOperator: Boolean,
+    ){
+        module.log.info("Disconnected! $client")
+        connections.remove(client);
+        for (room in rooms.keys) {
+            if (rooms[room]!!.tryRemove(client, isOperator)) {
+                return
+            }
         }
     }
 }

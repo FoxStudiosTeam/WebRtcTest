15c44cf4b42f7cd83c7f2e3e2ee286cf2f538e64
AgniaEndie <endienasg2@gmail.com>
2024-11-17 18:28:02 +0500

    build updated 9

--- room-service/src/main/kotlin/foxstudios/ru/config/Configuration.kt
+++ room-service/src/main/kotlin/foxstudios/ru/config/Configuration.kt
@@ -34,18 +34,18 @@ fun Application.configureHTTP() {
         header("Access-Control-Allow-Origin","*")
         header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
     }
-    install(CORS) {
-        allowMethod(HttpMethod.Options)
-        allowMethod(HttpMethod.Put)
-        allowMethod(HttpMethod.Delete)
-        allowMethod(HttpMethod.Patch)
-        allowMethod(HttpMethod.Post)
-        allowMethod(HttpMethod.Get)
-
-        allowHeader(HttpHeaders.Authorization)
-        allowHeader(HttpHeaders.ContentType)
-        allowHeader(HttpHeaders.Accept)
-
-        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
-    }
+//    install(CORS) {
+//        allowMethod(HttpMethod.Options)
+//        allowMethod(HttpMethod.Put)
+//        allowMethod(HttpMethod.Delete)
+//        allowMethod(HttpMethod.Patch)
+//        allowMethod(HttpMethod.Post)
+//        allowMethod(HttpMethod.Get)
+//
+//        allowHeader(HttpHeaders.Authorization)
+//        allowHeader(HttpHeaders.ContentType)
+//        allowHeader(HttpHeaders.Accept)
+//
+//        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+//    }
 }

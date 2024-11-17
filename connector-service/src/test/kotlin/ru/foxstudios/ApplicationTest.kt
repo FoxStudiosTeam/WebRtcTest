95fcf6f1575f71a7d376287162ea1cebc03886b5
AgniaEndie <endienasg2@gmail.com>
2024-11-17 16:57:13 +0500

    build updated 5

--- connector-service/src/test/kotlin/ru/foxstudios/ApplicationTest.kt
+++ connector-service/src/test/kotlin/ru/foxstudios/ApplicationTest.kt
@@ -7,14 +7,5 @@ import io.ktor.server.testing.*
 import kotlin.test.*
 
 class ApplicationTest {
-    @Test
-    fun testRoot() = testApplication {
-        application {
-            configureRouting()
-        }
-        client.get("/").apply {
-            assertEquals(HttpStatusCode.OK, status)
-            assertEquals("Hello World!", bodyAsText())
-        }
-    }
+
 }
